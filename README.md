09714ac288355c79abf292f145272188e279bfd7
Steve Byrne <sbb@penguinis.org>
2015-06-20 16:53:54 -0700

    More meaningful content in the README. Basic loader and combiner functions implemented.

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 The data for this project can be downloaded from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip.  Set the current directory to the directory of this project, and unzip the file.  Or, alternately, unzip it somewhere else convienient, and adjust the DATA.ROOT.DIRECTORY variable in run_analysis.R to specify where it was unzipped.
 
+The content in the zip file contains information about the origin of the data, and describes the feature names of the data values.
+
+The data are separated into training and test data sets.  
+
+=== Running the analysis
+
+To run the analysis, load the "run_analysis.R" file into R.  Be sure the working directory is set to the directory that contains the run_analysis.R file and (if you are sticking with the defaults), the unzipped data set.
+
+Invoke the XXX function.
+
--- run_analysis.R
+++ run_analysis.R
@@ -3,17 +3,56 @@
 
 DATA.ROOT.DIRECTORY = "UCI HAR Dataset"
 
+# Loads the names of the various activities from the activity_labels.txt file.  Returns a character vector containing
+# just the names
 load.activity.names <- function() {
   activity.names.filename <- file.path(DATA.ROOT.DIRECTORY, "activity_labels.txt")
   activity.names <- read.table(activity.names.filename, sep=" ", stringsAsFactors = FALSE)
   activity.names[,2]
 }
 
+# Loads the names of the columns in the primary dataset from features.txt
 load.data.column.names <- function() {
   data.column.names.filename = file.path(DATA.ROOT.DIRECTORY, "features.txt")
   column.names <- read.table(data.column.names.filename, sep=" ", stringsAsFactors = FALSE)
   column.names[,2]
 }
 
-print(load.activity.names())
-print(load.data.column.names())
+load.measurement.data <- function (data.type,...) {
+  measurement.filename = file.path(DATA.ROOT.DIRECTORY, data.type, paste0("X_", data.type, ".txt"))
+  data <- read.table(measurement.filename,  col.names=load.data.column.names(), ...)
+}
+
+# Loads the rows of activity records that correspond with the measurements.  Each row is an integer index that
+# corresponds to the activity labels.
+#
+# data.type is the "type" of the data, either "test" or "training"
+# Returns a vector of integers
+load.activity.records <- function(data.type) {
+  activity.indices.filename = file.path(DATA.ROOT.DIRECTORY, data.type, paste0("y_", data.type, ".txt"))
+  activity.indices <- read.table(activity.indices.filename, sep=" ", stringsAsFactors = FALSE)
+  activity.indices[,1]
+}
+
+load.subject.numbers <- function(data.type, ...) {
+  activity.indices.filename = file.path(DATA.ROOT.DIRECTORY, data.type, paste0("subject_", data.type, ".txt"))
+  activity.indices <- read.table(activity.indices.filename, ...)
+  activity.indices[,1]
+}
+
+load.combined.values <- function(loader.func, ...) {
+  first.data.set <- loader.func("train", ...)
+  print(c("first data", dim(first.data.set)))
+  second.data.set <- loader.func("test", ...)
+  combining.func <- if (is.null(dim(first.data.set))) append else rbind
+  
+  combining.func(first.data.set, second.data.set)
+}
+
+#print(class(load.activity.names()))
+#print(load.data.column.names())
+#data <- load.measurement.data("test", nrows = 10)
+#print(class(data))
+#print(load.activity.records("test"))
+#print(load.subject.numbers("test"))
+print(load.combined.values(load.measurement.data, nrow=3))
