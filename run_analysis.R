d0f21b9557fead36fb7c6dcfbd7a6ca2ea2bec9a
Steve Byrne <sbb@penguinis.org>
2015-06-20 15:03:33 -0700

    First function implemented.  Just a work in progress commit.

--- run_analysis.R
+++ run_analysis.R
@@ -1,2 +1,12 @@
 # Run an analysis of the inertial measurement data.
 # Combines the data from the training and test sets
+
+DATA.ROOT.DIRECTORY = "UCI HAR Dataset"
+
+load.activity.labels <- function() {
+  activity.labels.filename <- file.path(DATA.ROOT.DIRECTORY, "activity_labels.txt")
+  activity.labels <- read.table(activity.labels.filename, sep=" ", stringsAsFactors = FALSE)
+  activity.labels[,2]
+}
+
+print(load.activity.labels())
